cdec06397bd1edd08d2a4c6cc136a3cba5d18697
Lizazhigulova <49558507+Lizazhigulova@users.noreply.github.com>
2023-02-05 14:42:47 +0000

    Адреса студий

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,15 +1,17 @@
 import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
+        <div className='d-inline-flex flex-column gap-5 mt-4 borderFrame '>
+            <div className='title'>Адреса заведений</div>
+            <div className='d-inline-flex flex-column gap-4'>
+               <div className="text-rg pt-2 pb-2"><ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement></div>
+               <div className="text-rg pt-2 pb-2"><ListElement>Санкт-Петербург, Казанская ул, 7</ListElement></div>
             </div>
+            <div className="mt-auto gap-4">
+            <SecondaryButton>Закрыть запись</SecondaryButton></div>
         </div>
     );
 }
